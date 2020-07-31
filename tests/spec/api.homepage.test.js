815c76a968e0da232b6466a082df57e8398bfeda
bednaJedna <bednarik.radek@gmail.com>
2020-07-31 11:07:46 +0200

    update

--- tests/spec/api.homepage.test.js
+++ tests/spec/api.homepage.test.js
@@ -1,3 +1,5 @@
+// just to try API testing in node/javascript env and tools
+// not related to Playwright per se
 const Homepage = require("../pageobjects/homepage.page");
 const expect = require("chai").expect;
 const chai = require("chai");
@@ -6,17 +8,15 @@ const chaiHttp = require("chai-http");
 const domain = "https://www.tesena.com";
 chai.use(chaiHttp);
 
-Homepage.browsers.forEach(function (browser) {
-  describe(`${browser}: api tests`, function () {
-    it(`GET ${Homepage.url} returns 200`, function (done) {
-      chai
-        .request(domain)
-        .get("/en")
-        .end(function (err, res) {
-          if (err) done(err);
-          expect(res).to.have.status(200);
-          done();
-        });
-    });
+describe("API tests", function () {
+  it(`GET ${Homepage.url} returns 200`, function (done) {
+    chai
+      .request(domain)
+      .get("/en")
+      .end(function (err, res) {
+        if (err) done(err);
+        expect(res).to.have.status(200);
+        done();
+      });
   });
 });
